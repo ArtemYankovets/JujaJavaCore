c757ca891cadd0c753bbf22eb4d00976709b1892
artemyankovets <artemyankovets@gmail.com>
2016-06-02 21:29:53 +0300

    week 10 IO Streams

---
+++ week10/src/main/java/ua/com/juja/lab45/ByteFilter.java
@@ -0,0 +1,12 @@
+package ua.com.juja.lab45;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class ByteFilter {
+    public static void filter(InputStream src, OutputStream dst, int bufferSize, byte filterCriteria) throws IOException {
+        /*BODY*/
+    }
+
+}
