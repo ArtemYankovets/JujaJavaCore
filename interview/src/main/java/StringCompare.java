78706a29da8177a21635ee7cdf0ff73adeee7ace
ArtemYankovets <ArtemYankovets@gmail.com>
2016-07-21 12:33:21 +0300

    interview String compare

--- interview/src/main/java/StringCompare.java
+++ interview/src/main/java/StringCompare.java
@@ -1,18 +1,24 @@
+import java.io.*;
 
 public class StringCompare {
 
-    public static void main(String[] args) {
-//        String input = "some test input";
-//        String expected = "manually reversed string";
+    public static void main(String[] args) throws UnsupportedEncodingException {
 
-        char[] arr = {'e', 'v', 'o', 'l', '4'};
-        String input = "evol4";
-        String expected = "4l"+"ove";
+        String input = "qwerty\uD83F\uDFFA";
+        String expected = "\uD83F\uDFFAytrewq";
 
-        System.out.println("input " + input);
-        System.out.println("expected " + expected);
+        System.out.println("input \t\t" + input);
+        System.out.println("expected \t" + expected);
 
-        System.out.println(expected.equals(reverse(input)));
+        String result = reverse(input);
+        System.out.println("reverse result \t\t\t\t" + result);
+
+        String result2 = reverseByArray(input);
+        System.out.println("reverseByArray result \t\t" + result2);
+
+        System.out.println();
+        System.out.println(expected.equals(result));
+        System.out.println(expected.equals(result2));
 
     }
 
@@ -23,5 +29,16 @@ public class StringCompare {
             b[(a.length - 1) - i] = a[i];
         }
         return new String(b);
+    }
+
+    public static String reverseByArray(String s) throws UnsupportedEncodingException {
+        byte [] a = new byte[0];
+        a = s.getBytes("UTF-32");
+        byte[] b = new byte[a.length];
+        int sizeChar = a.length / 4;
+        for (int i = 0; i < sizeChar; i++) {
+            System.arraycopy(a, i*4, b, (sizeChar - 1 - i)*4, 4);
+        }
+        return new String(b, "UTF-32");
     }
 }
